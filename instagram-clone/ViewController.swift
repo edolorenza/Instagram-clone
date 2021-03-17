eb423cab50ee675e0f81cde82ef3ceca6d675166
Edo Lorenza <edolrnz@gmail.com>
2021-03-17 20:13:35 +0700

    Initial Commit

---
+++ instagram-clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  instagram-clone
+//
+//  Created by Edo Lorenza on 17/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
