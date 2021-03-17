79e3cc0515624fab72f32eb38a1c38e0fc8ac264
Edo Lorenza <edolrnz@gmail.com>
2021-03-17 20:27:58 +0700

    configure project

---
+++ instagram-clone/Controller/MainTabController.swift
@@ -0,0 +1,15 @@
+//
+//  MainTabController.swift
+//  instagram-clone
+//
+//  Created by Edo Lorenza on 17/03/21.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
--- instagram-clone/SceneDelegate.swift
+++ instagram-clone/SceneDelegate.swift
@@ -13,10 +13,15 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+       
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        /* MARK: Untuk melihat screen yang sedang dikerjakan, ganti parameter rootViewController dari UINavigationController menjadi nama Controller yang ingin dilihat.
+         
+         Contoh: self.window?.rootViewController = UINavigationController(rootViewController: <Nama Controller yang ingin dilihat>()) */
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController =  MainTabController()
+        window?.makeKeyAndVisible()
+        
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
