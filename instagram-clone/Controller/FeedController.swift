45e76e46fe0f2229925c798f2c8905249934c716
Edo Lorenza <edolrnz@gmail.com>
2021-03-18 10:19:39 +0700

    setting up collectionview programmatically

--- instagram-clone/Controller/FeedController.swift
+++ instagram-clone/Controller/FeedController.swift
@@ -7,9 +7,40 @@
 
 import UIKit
 
-class FeedController: UIViewController {
+
+class FeedController: UICollectionViewController {
+    private let reuseIdentifier = "Cell"
+    
+    //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
+        configureUI()
+    }
+    
+    //MARK: - helpers
+    func configureUI() {
+        collectionView.backgroundColor = .white
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+        
+    }
+   
+}
+//MARK: - UICollectionViewDataSource
+extension FeedController {
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        cell.backgroundColor = .red
+        return cell
+    }
+}
+
+//MARK: - UICollectionViewDelegateFlowLayout
+extension FeedController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 350)
     }
 }
