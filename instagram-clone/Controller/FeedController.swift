7271a85eb2b7bd56a9777d730447217f6f91869e
Edo Lorenza <edolrnz@gmail.com>
2021-03-19 10:31:49 +0700

    finising up feed cell UI

--- instagram-clone/Controller/FeedController.swift
+++ instagram-clone/Controller/FeedController.swift
@@ -40,6 +40,11 @@ extension FeedController {
 //MARK: - UICollectionViewDelegateFlowLayout
 extension FeedController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: view.frame.width, height: 350)
+        
+        let width = view.frame.width
+        var height = width + 8 + 40 + 8
+        height += 110
+        
+        return CGSize(width: width, height: height)
     }
 }
--- instagram-clone/View/FeedCell.swift
+++ instagram-clone/View/FeedCell.swift
@@ -28,6 +28,60 @@ class FeedCell: UICollectionViewCell{
         return button
     }()
     
+    private let postImageView: UIImageView =  {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        iv.isUserInteractionEnabled = true
+        iv.image = #imageLiteral(resourceName: "avenger")
+        return iv
+    }()
+    
+    private lazy var likeButton: UIButton =  {
+        let button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName: "like_unselected"), for: .normal)
+        button.tintColor = .black
+        return button
+    }()
+    
+    private lazy var commentButton: UIButton =  {
+        let button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName: "comment"), for: .normal)
+        button.tintColor = .black
+        return button
+    }()
+    
+    private lazy var shareButton: UIButton =  {
+        let button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName: "send2"), for: .normal)
+        button.tintColor = .black
+        return button
+    }()
+    
+    
+    private let likeLabel: UILabel =  {
+        let label = UILabel()
+        label.text = "1 like"
+        label.font = UIFont.boldSystemFont(ofSize: 13)
+        return label
+    }()
+    
+    private let captionLabel: UILabel =  {
+        let label = UILabel()
+        label.text = "Avenger Endgame ...."
+        label.font = UIFont.systemFont(ofSize: 14)
+        return label
+    }()
+    
+    private let postTimeLabel: UILabel =  {
+        let label = UILabel()
+        label.text = "Two days ago"
+        label.font = UIFont.systemFont(ofSize: 12)
+        label.alpha = 0.5
+        return label
+    }()
+    
+    private var stackView = UIStackView()
     //MARK: - lifecycle
     
     override init (frame: CGRect){
@@ -44,6 +98,21 @@ class FeedCell: UICollectionViewCell{
         usernameButton.centerY(inView: profileImageView,
                                leftAnchor: profileImageView.rightAnchor,
                                paddingLeft: 12)
+        
+        self.addSubview(postImageView)
+        postImageView.anchor(top:profileImageView.bottomAnchor, left: leftAnchor, right: rightAnchor, paddingTop: 8)
+        postImageView.heightAnchor.constraint(equalTo: widthAnchor, multiplier: 1).isActive = true
+        
+        configureActionButton()
+        
+        self.addSubview(likeLabel)
+        likeLabel.anchor(top: stackView.bottomAnchor, left: leftAnchor, paddingTop: -4, paddingLeft: 10)
+        
+        self.addSubview(captionLabel)
+        captionLabel.anchor(top: likeLabel.bottomAnchor, left: leftAnchor, paddingTop: 4, paddingLeft: 10)
+        
+        self.addSubview(postTimeLabel)
+        postTimeLabel.anchor(top: captionLabel.bottomAnchor, left: leftAnchor, paddingTop: 4, paddingLeft: 10)
     }
     
     required init?(coder: NSCoder) {
@@ -53,5 +122,16 @@ class FeedCell: UICollectionViewCell{
     //MARK: - Actions
     @objc func didTapUsername() {
         
+    }
+    
+    //MARK: - helpers
+    
+    func configureActionButton(){
+        stackView = UIStackView(arrangedSubviews: [likeButton, commentButton, shareButton])
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        
+        self.addSubview(stackView)
+        stackView.anchor(top: postImageView.bottomAnchor, width: 120, height: 50)
     }
 }
