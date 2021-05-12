d2f773190d4304d47daf28df2ab2060229121bc3
edo Lorenza <edolrnz@gmail.com>
2021-05-12 15:48:38 +0700

    add UI : notification controller

--- instagram-clone/Controller/NotificationController.swift
+++ instagram-clone/Controller/NotificationController.swift
@@ -7,9 +7,42 @@
 
 import UIKit
 
-class NotificationController: UIViewController {
+class NotificationController: UITableViewController {
+    //MARK: - Properties
+    private let reuseIdentifier = "NotificationCell"
+    
+    //MARK: - Lifycycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .magenta
+        configureTableView()
+    }
+    
+    //MARK: - API
+    
+    //MARK: - Action
+    
+    //MARK: - Helpers
+    func configureTableView() {
+        view.backgroundColor = .white
+        navigationItem.title = "Notifications"
+        tableView.register(NotificationCell.self, forCellReuseIdentifier: reuseIdentifier)
+        tableView.rowHeight = 80
+        tableView.separatorStyle = .none
     }
 }
+
+//MARK: - TableViewDataSource
+extension NotificationController {
+  
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! NotificationCell
+        return cell
+    }
+}
+
+
+
