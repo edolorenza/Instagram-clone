a717aa11c26ffdb85836ea244909553b5ec9cb09
edo Lorenza <edolrnz@gmail.com>
2021-05-16 11:04:03 +0700

    bug fix : Notification ID

--- instagram-clone/API/NotificationService.swift
+++ instagram-clone/API/NotificationService.swift
@@ -13,14 +13,21 @@ struct NotificationService {
     static func uploadNotification(toUid uid: String, type: NotificationType, post: Post? = nil) {
         guard let currentUid = Auth.auth().currentUser?.uid else { return }
         guard  uid != currentUid else { return }
+        
+        let docRef = COLLECTION_NOTIFICATIONS.document(uid).collection("user-notifications").document()
+       
+        
+        
         var data: [String: Any] = ["timestamp": Timestamp(date: Date()),
                                    "uid": currentUid,
-                                   "type": type.rawValue]
+                                   "type": type.rawValue,
+                                   "id": docRef.documentID    ]
         if let post = post {
             data["postId"] = post.postId
             data["postImageUrl"] = post.imageUrl
         }
-        COLLECTION_NOTIFICATIONS.document(uid).collection("user-notifications").addDocument(data: data)
+        
+        docRef.setData(data)
     }
     
     static func fetchNotifications() {
